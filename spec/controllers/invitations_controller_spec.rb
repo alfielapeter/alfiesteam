9dc4419cc2a06fa81bb2c93ed3a218b921d35e30
Alfie La Peter <alfie@alfielapeter.com>
2011-02-09 14:42:05 -0700

    first commit

---
+++ spec/controllers/invitations_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe Users::InvitationsController do
+	
+	describe "Create" do
+		it "should really write a test for that complex create method"
+	end
+	
+	describe "Edit" do
+		it "should set the session variable for team_id"
+	end
+end
