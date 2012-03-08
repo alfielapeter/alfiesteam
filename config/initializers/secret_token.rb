8d58eefd47e00f974504c0d6b194dc291ba7a9d1
Alfie La Peter <alfie@alfielapeter.com>
2012-03-07 19:26:24 -0700

    changed one character in secret token to fix devise

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Alfiesteam::Application.config.secret_token = 'b3d5ede266bdadf41496601f08d65f3a9196409e51430011a3ad72195a33b2003d0f43cd129210db886a274753a159a46acbe52e141d3194d22b227372597cc0'
+Alfiesteam::Application.config.secret_token = 'b3d5ede266bdadf41496601f08d65f3a9196409e51430011b3ad72195a33b2003d0f43cd129210db886a274753a159a46acbe52e141d3194d22b227372597cc0'
