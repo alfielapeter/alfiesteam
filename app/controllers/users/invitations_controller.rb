6d2f874fb0814ce21221dae11e3e80ecb4ec17d1
Alfie La Peter <alfie@alfielapeter.com>
2012-03-25 21:10:17 -0600

    fixed invitation bug

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,36 +1,37 @@
 class Users::InvitationsController < Devise::InvitationsController
-	
-	def create
-		if User.exists?(:email => params[:user][:email])
-			@player = User.find_by_email(params[:user][:email])
-			if current_team.users.include?(@player)
-				set_flash_message :alert, "That player is already on this team."
-			else
-				current_team.users << @player
-				current_team.games.each {|g| GamesUser.create(:game_id => g.id, :user_id => @player.id)}
-				set_flash_message :notice, "Good news! That player is already in our system and has been added to your team."
-    	end
-			redirect_to team_path(current_team)
-		else
-			@player = User.invite!(params[:user])
-		
-    	if @player.errors.empty?
-				current_team.users << @player
-				current_team.games.each {|g| GamesUser.create(:game_id => g.id, :user_id => @player.id)}
-	      set_flash_message :notice, :send_instructions
-	      redirect_to team_path(current_team)
-			else
-				set_flash_message :alert, "#{@player.errors.full_messages}. Please try inviting that player again."
-	      redirect_to :back
-	    end
-		end
-	end
-	
-	def edit
+
+  def create
+    if User.exists?(:email => params[:user][:email])
+      @player = User.find_by_email(params[:user][:email])
+      if current_team.users.include?(@player)
+        set_flash_message :alert, "That player is already on this team."
+      else
+        current_team.users << @player
+        current_team.games.each {|g| GamesUser.create(:game_id => g.id, :user_id => @player.id)}
+        set_flash_message :notice, "Good news! That player is already in our system and has been added to your team."
+      end
+      redirect_to team_path(current_team)
+    else
+      @player = User.invite!(params[:user])
+
+      if @player.errors.empty?
+        @player.update_attribute(:time_zone, current_user.time_zone)
+        current_team.users << @player
+        current_team.games.each {|g| GamesUser.create(:game_id => g.id, :user_id => @player.id)}
+        set_flash_message :notice, :send_instructions
+        redirect_to team_path(current_team)
+      else
+        set_flash_message :alert, "#{@player.errors.full_messages}. Please try inviting that player again."
+        redirect_to :back
+      end
+    end
+  end
+
+  def edit
     if params[:invitation_token] && self.resource = resource_class.first(:conditions => { :invitation_token => params[:invitation_token] })
       @team = self.resource.teams.last
-			@game = @team.games.where("start_at > ?", Time.now).order(:start_at).first rescue nil
-			render_with_scope :edit
+      @game = @team.games.where("start_at > ?", Time.now).order(:start_at).first rescue nil
+      render_with_scope :edit
     else
       set_flash_message(:alert, :invitation_token_invalid)
       redirect_to after_sign_out_path_for(resource_name)
@@ -43,10 +44,12 @@ class Users::InvitationsController < Devise::InvitationsController
     if resource.errors.empty?
       set_flash_message :notice, :updated
       sign_in_and_redirect(resource_name, resource)
-			session[:team_id] = current_user.teams.last.id
+      session[:team_id] = current_user.teams.last.id
     else
+      @team = self.resource.teams.last
+      @game = @team.games.where("start_at > ?", Time.now).order(:start_at).first rescue nil
       render_with_scope :edit
     end
   end
-	
+
 end
